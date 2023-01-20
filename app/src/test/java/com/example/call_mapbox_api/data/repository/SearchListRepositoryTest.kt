ac75cc72ee2aa36d2388e8529fce1cee7de17483
sollarp <szpitor@gmail.com>
2023-01-20 22:03:11 +0000

    Test repository added

--- app/src/test/java/com/example/call_mapbox_api/data/repository/SearchListRepositoryTest.kt
+++ app/src/test/java/com/example/call_mapbox_api/data/repository/SearchListRepositoryTest.kt
@@ -2,8 +2,12 @@ package com.example.call_mapbox_api.data.repository
 
 import com.example.call_mapbox_api.data.IEvPointLocalDataSource
 import com.example.call_mapbox_api.data.IEvPointRemoteDataSource
+import com.example.call_mapbox_api.fakeData.fakeEvPointsEntity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -12,6 +16,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -29,12 +35,24 @@ class SearchListRepositoryTest {
     lateinit var evPointLocalDataSource: IEvPointLocalDataSource
 
     @Test
-    fun `Check local and remote database items in Flow`() {
+    fun `Check local and remote data source objects`() {
 
         runBlocking {
-            val firstItem = searchListRepository.fetchList()
-            assertEquals(evPointRemoteDataSource.getLatestEvPoint(), firstItem)
-            assertEquals(evPointLocalDataSource.fetchPoints(), firstItem)
+            val actual = searchListRepository.fetchList()
+            assertEquals(evPointRemoteDataSource.getLatestEvPoint(), actual)
+            assertEquals(evPointLocalDataSource.fetchPoints(), actual)
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Test localDataSource entities and verify one item`() {
+        runTest {
+            `when`(evPointLocalDataSource.fetchPoints()).thenReturn(fakeEvPointsEntity())
+            val actual = searchListRepository.fetchList().toList()[0]
+                .toList().map { it.NumberOfPoints }
+            val expected = fakeEvPointsEntity().toList()[0].map { it.NumberOfPoints }
+            assertEquals(expected, actual)
         }
     }
 }
--- app/src/test/java/com/example/call_mapbox_api/domain/SearchListUseCaseTest.kt
+++ app/src/test/java/com/example/call_mapbox_api/domain/SearchListUseCaseTest.kt
@@ -4,6 +4,7 @@ import com.example.call_mapbox_api.data.remote.AddressInfo
 import com.example.call_mapbox_api.data.remote.Connections
 import com.example.call_mapbox_api.data.remote.EvPointsEntity
 import com.example.call_mapbox_api.data.repository.ISearchListRepository
+import com.example.call_mapbox_api.fakeData.fakeEvPointsEntity
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.test.*
@@ -26,57 +27,6 @@ class SearchListUseCaseTest {
     @Before
     fun setUp() {
         searchListUseCase = SearchListUseCase(searchListRepository, Dispatchers.Default)
-    }
-
-    private fun fakeEvPointsEntity(): Flow<List<EvPointsEntity>> {
-
-        val fakeAddressInfo = AddressInfo(
-            AddressLine1 = "add1",
-            AddressLine2 = "add2",
-            CountryID = 4,
-            DistanceUnit = 5,
-            Distance = 3.4,
-            ID = 4,
-            Latitude = 5.6,
-            Longitude = 3.2,
-            Postcode = "postcode",
-            StateOrProvince = "state",
-            Title = "title",
-            Town = "town"
-        )
-
-        val fakeConnetion = Connections(
-            ConnectionTypeID = 3,
-            CurrentTypeID = 4,
-            ID = 7,
-            Voltage = 7,
-            Amps = 9,
-            LevelID = 0,
-            PowerKW = 1.4,
-            Quantity = 8,
-            StatusTypeID = 8
-        )
-        val fakeEvPointsEntity = EvPointsEntity(
-            ID = 4,
-            DateLastVerified = "Datalast",
-            DataProviderID = 5,
-            DataQualityLevel = 8,
-            DateCreated = "datacreated",
-            DateLastStatusUpdate = "date",
-            NumberOfPoints = 5,
-            IsRecentlyVerified = true,
-            OperatorID = 3,
-            StatusTypeID = 5,
-            SubmissionStatusTypeID = 5,
-            UUID = "uuid",
-            UsageCost = "usagecostmoney",
-            UsageTypeID = 5,
-            Connections = listOf(fakeConnetion),
-            AddressInfo = fakeAddressInfo
-        )
-        return flow {
-            emit(listOf(fakeEvPointsEntity))
-        }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
